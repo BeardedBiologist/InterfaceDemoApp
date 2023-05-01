ac62e445c0b7b792cc3e99cecfa6cb78970f5909
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-01 20:51:52 +0200

    finished lesson

--- InterfaceDemo/Program.cs
+++ InterfaceDemo/Program.cs
@@ -14,9 +14,105 @@ namespace InterfaceDemo
     {
         static void Main(string[] args)
         {
-            
+
+            List<IComputerController> controllers = new List<IComputerController>();
+
+            Keyboard keyboard = new Keyboard();
+            GameController gameController = new GameController();
+            BatteryPowerGameController battery = new BatteryPowerGameController();
+            BatteryPowerKeyboard batterykeyboard = new BatteryPowerKeyboard();
+
+            controllers.Add(keyboard);
+            controllers.Add(gameController);
+
+            foreach (IComputerController controller in controllers)
+            {
+                if (controller is GameController gc)
+                {
+                    
+                }
+
+                if (controller is IBatteryPowered powered)
+                {
+
+                }
+            }
+            //using IDisposable - dispose (power of interfaces) 
+            using (GameController gc = new GameController())
+            {
+
+            }
+
+            List<IBatteryPowered> powereds = new List<IBatteryPowered>();
+
+            powereds.Add(battery);
+            powereds.Add(batterykeyboard);
 
             Console.ReadLine();
         }
+    }
+
+    public interface IComputerController : IDisposable
+    {
+        void Connect();
+        void CurrentKeyPressed();
+    }
+
+
+        public class Keyboard : IComputerController
+    {
+        public void Connect()
+        {
+
+        }
+
+        public void CurrentKeyPressed()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConnectionType { get; set; }
+
+    }
+
+    public class GameController : IComputerController, IDisposable
+    {
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CurrentKeyPressed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            //do whatever shutdown tasks needed
+        }
+
+        public int BatteryLevel { get; set; }
+
+    }
+
+    public interface IBatteryPowered
+    {
+        int BatteryLevel { get; set; }
+    }
+
+    public class BatteryPowerKeyboard : Keyboard, IBatteryPowered
+    {
+        public int BatteryLevel { get; set; }
+    }
+
+    public class BatteryPowerGameController : GameController, IBatteryPowered
+    {
+        public int BatteryLevel { get; set; }
     }
 }
