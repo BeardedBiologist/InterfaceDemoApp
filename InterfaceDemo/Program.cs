818840e9d6663657d0dd985b279aa2615e6f25d3
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-01 20:23:16 +0200

    Init Commit

--- InterfaceDemo/Program.cs
+++ InterfaceDemo/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace InterfaceDemo
+{
+    // An interface is a contract
+    // Classes implement interfaces
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadLine();
+        }
+    }
+}
